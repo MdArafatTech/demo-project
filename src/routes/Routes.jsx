5dc0e0ce81dbdf0619604c2efaff5bb7140e5364
Arafat <mdalarafatabir@gmail.com>
2025-07-27 08:01:52 +0600

    first commit

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,12 +1,12 @@
 
 import { createBrowserRouter } from 'react-router';
 import Root from '../layout/Root';
-import From from '../pages/Form';
-import Register from '../pages/Register';
+
+
 import Errorpage from '../pages/errorPage';
-import Account from '../pages/Account';
-import Contact from '../pages/Contact';
+
 import HappyBirthday from '../pages/HappyBirthday';
+
 
 
 
@@ -18,22 +18,9 @@ const Routes = createBrowserRouter(
            element:<Root></Root>,
            errorElement:<Errorpage></Errorpage>,
            children:[
-            {
-              path:"/register",
-              element:<Register></Register>
-            },
-           {
-            path:"/form",
-            element:<From></From>
-           },
-          {
-            path:"/account",
-            element:<Account></Account>
-          },
-          {
-            path:"/contact",
-            element:<Contact></Contact>
-          },
+            
+          
+          
           {
             path:"/happybirthday",
             element:<HappyBirthday></HappyBirthday>
